b8c86b84d79fe69dbfb0a5c60e3e86d49a73d056
Tawqeer Ali <105933510+tawqeer00@users.noreply.github.com>
2024-05-24 23:12:46 -0800

    Update constants.js

--- src/components/data/constants.js
+++ src/components/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/tawqeer00",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1418vTXlLDZww1xFxyraWtNALczdZWQmr/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/tawqeer-ali-795745210/",
   twitter: "https://twitter.com/tawqeer00",
   insta: "https://www.instagram.com/tawqeer_ali/",
